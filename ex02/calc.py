38e21ac51fb672766a5fc60c1033991be96ac4d4
C0A21037 <c0a210372f@edu.teu.ac.jp>
2022-11-29 14:39:38 +0900

    練習問題4,5

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,30 +2,39 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 
-
+# 練習３
 def button_click(event):
     btn = event.widget
-    txt = btn["text"]
-    tkm.showinfo(txt,f"[{txt}]ボタンが押されました")
+    num = btn["text"]
+    tkm.showinfo("", f"{num}ボタンがクリックされました")
 
+    
+# 練習１
 root = tk.Tk()
 root.geometry("300x500")
 
-r,c=0,0
-for num in range(9,-1,-1):
-    button = tk.Button(root,text=f"{num}",font=("",30),width=4,height=2)
-    button.bind("<1>",button_click)
-    button.grid(row=r,column=c)
-    c+=1
-    if c%3==0:
+# 練習４
+entry = tk.Entry(root, justify="right", width=10, font=("",40))
+entry.grid(row=0, column=0, columnspan=3)
+
+# 練習２
+r, c = 1, 0
+for num in range(9, -1, -1):
+    button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
+    button.grid(row=r, column=c)
+    button.bind("<1>", button_click)
+    c += 1
+    if c%3 == 0:
         r += 1
-        c=0
+        c = 0
 
-
-
-
-
-
-
+operators = ["+", "="]
+for ope in operators:
+    button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
+    button.grid(row=r, column=c)
+    c += 1
+    if c%3 == 0:
+        r += 1
+        c = 0
 
 root.mainloop()
