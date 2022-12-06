57ed58529008385903d9051a13c729e9d2b19e88
C0A21037 <c0a210372f@edu.teu.ac.jp>
2022-12-06 14:28:40 +0900

    練習3

--- ex03/fig/maze.py
+++ ex03/fig/maze.py
@@ -5,4 +5,7 @@ if __name__ == "__main__":
     root.title("迷えるこうかとん")
     canvas=tk.Canvas(root,width=1500,height=900,bg="black")
     canvas.pack()
+    cx,cy=300,400
+    tori = image=tk.PhotoImage(file="ex03/fig/8.png")
+    canvas.create_image(cx,cy,image=tori,tag="kokaton")
     root.mainloop()
