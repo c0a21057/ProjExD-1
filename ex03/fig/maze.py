5dd10b588503b73be3e641ba2eba32ff7924e9a7
C0A21037 <c0a210372f@edu.teu.ac.jp>
2022-12-06 17:08:50 +0900

    実装

--- ex03/fig/maze.py
+++ ex03/fig/maze.py
@@ -10,7 +10,6 @@ def key_up(event):
     global key
     key = ""
 
-
 def main_proc():
     global cx, cy,mx,my
     if key == "Up": my -= 1
@@ -22,7 +21,6 @@ def main_proc():
         if key == "Down": my -= 1
         if key == "Left": mx += 1
         if key == "Right": mx -= 1
-
     cx, cy = mx*100+50,my*100+50
     canvas.coords("kokaton", cx, cy)
     root.after(100, main_proc)
@@ -33,15 +31,15 @@ if __name__ == "__main__":
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
-
     maze_lst = mm.make_maze(15, 9)
     # print(maze_lst)
     mm.show_maze(canvas, maze_lst)
 
     mx,my= 1,1
     cx, cy = mx*100+50,my*100+50
+
     tori = tk.PhotoImage(file="ex03/fig/8.png")
-    canvas.create_image(cx, cy, image=tori, tag="kokaton")
+    canvas.create_image(cx, cy, image=tori, tag="kokaton") 
     key = ""
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
