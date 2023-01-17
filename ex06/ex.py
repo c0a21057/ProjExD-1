6274ec18f7c4163a5904d5738df7ad2b92f299d4
C0A21087 <c0a21087f6@edu.teu.ac.jp>
2023-01-17 14:40:07 +0900

    3回目

--- ex06/ex.py
+++ ex06/ex.py
@@ -60,8 +60,69 @@ def main2():
     arr[foodx][foody] = -1
     snake_lon = 3  # 蛇の長さ
     way = 1  # 蛇の運動方向
+    spd = 1 # 蛇の移動速度(筒井)
+    t = 0.1 # 蛇の移動速度変更用の変数(筒井)
 
+    while True:
+        scr.blit()
+        maze.blit(scr)
+        
+        #screen.fill(color_white)
+        time.sleep(0.1)
+        for event in pg.event.get():  
+            if event.type == pg.QUIT:
+                sys.exit()
+            elif event.type == pg.KEYDOWN:
+                if (event.key == pg.K_RIGHT) and (way != 2):  # 右
+                    way = 1
+                if (event.key == pg.K_LEFT) and (way != 1):  # 左
+                    way = 2
+                if (event.key == pg.K_UP) and (way != 4):  # 上
+                    way = 3
+                if (event.key == pg.K_DOWN) and (way != 3):  # 下に移動
+                    way = 4
+        if way == 1:
+            x += spd # 1をspdに変更(筒井)
+        if way == 2:
+            x -= spd
+        if way == 3:
+            y -= spd
+        if way == 4:
+            y += spd
+        if (x > 60) or (y > 40) or (x < 1) or (y < 1) or (arr[x][y] > 0):  # 死亡(壁、自分の体をぶつかったら)
+            sys.exit()
+        arr[x][y] = snake_lon
+        for a, b in enumerate(arr, 1):
+            for c, d in enumerate(b, 1):
+                # 食べ物は-1，空地は0，蛇の位置は正数
+                if (d > 0):
+                    # print(a,c) #蛇の座標を表示
+                    arr[a - 1][c - 1] = arr[a - 1][c - 1] - 1
+                    pg.draw.rect(screen, color_green, ((a - 1) * 10, (c - 1) * 10, 10, 10))
+                if (d < 0):
+                    pg.draw.rect(screen, color_red, ((a - 1) * 10, (c - 1) * 10, 10, 10))  
+        # 蛇の移動速度(筒井)
+        for event in pg.event.get(): 
+            if event.type == pg.QUIT:
+                sys.exit()
+            elif event.type == pg.KEYDOWN:
+                if (event.key == pg.K_RIGHT) and (way != 2):  # 右
+                    way = 1
+                if (event.key == pg.K_LEFT) and (way != 1):  # 左
+                    way = 2
+                if (event.key == pg.K_UP) and (way != 4):  # 上
+                    way = 3
+                if (event.key == pg.K_DOWN) and (way != 3):  # 下に移動
+                    way = 4
 
+        if (x == foodx) and (y == foody):   #蛇が食べ物を食べたら
+            snake_lon += 1    #長さ+1
+            t -= t/4 # 蛇が現在の速度の1/4加速(筒井)
+
+            while (arr[foodx][foody] != 0):    #新しい食べ物を表示
+                foodx = random.randint(1, 60)
+                foody = random.randint(1, 40)
+            arr[foodx][foody] = -1
 
         pg.display.update()
         clock.tick(1000)
